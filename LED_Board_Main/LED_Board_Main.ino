9f3af4ecbe6093681fcdb6ea861de18a1dae8631
Tim MacMannis <timmacmannis@yahoo.com>
2020-10-09 16:21:13 -0400

    all libraries working together!

--- LED_Board_Main/LED_Board_Main.ino
+++ LED_Board_Main/LED_Board_Main.ino
@@ -1,19 +1,23 @@
 #include <Adafruit_GFX.h>
 #include <FastLED.h>
 #include <FastLED_NeoMatrix.h>
+#include <LEDMatrix.h>
 
 #define PIN 12
-#define mw 32
-#define mh 16
-#define NUMMATRIX (mw*mh)
+#define MATRIX_WIDTH 32
+#define MATRIX_HEIGHT 16
+#define NUMMATRIX (MATRIX_WIDTH*MATRIX_HEIGHT)
+#define MATRIX_TYPE    (MTX_MATRIX_TOP + MTX_MATRIX_LEFT + MTX_MATRIX_ROWS + MTX_MATRIX_PROGRESSIVE)
 
 int brightness = 127;
 int lastBrightness = 0;
 
+cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> leds;
 
-CRGB matrixleds[NUMMATRIX];
 
-FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(matrixleds, mw, mh, 1, 1, 
+CRGB *matrixleds = leds[0];
+
+FastLED_NeoMatrix *matrix = new FastLED_NeoMatrix(matrixleds, MATRIX_WIDTH, MATRIX_HEIGHT, 1, 1, 
   NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
     NEO_MATRIX_ROWS + NEO_MATRIX_PROGRESSIVE );
 
@@ -22,6 +26,7 @@ const uint16_t colors[] = {
 
 void setup() {
   Serial.begin(57600);
+  while (!Serial) {;}
   FastLED.addLeds<WS2812B,PIN,RGB>(matrixleds, NUMMATRIX); 
   matrix->begin();
   matrix->setTextWrap(false);
@@ -30,12 +35,13 @@ void setup() {
 }
 
 void loop() {
-//	checkForUpdates();
-//	testAnimation(random(5000, 20000), millis());
+	checkForUpdates();
+	// testAnimation(random(5000, 20000), millis());
 //	showFireworks(0, random(3, 15));
+
   fadeToBlackBy( matrixleds, NUMMATRIX, 10);
   int pos = random16(NUMMATRIX);
-  matrixleds[pos] += CHSV( 50 + random8(64), 200, 255);
+  matrixleds[pos] += CHSV( 50 + random8(64), 200, brightness);
   matrix->show();
   FastLED.delay(1000/120);
 
--- LED_Board_Main/testAnimation.ino
+++ LED_Board_Main/testAnimation.ino
@@ -1,59 +1,59 @@
-// uint8_t hue = 0;
-// int16_t counter = 0;
+uint8_t hue = 0;
+int16_t counter = 0;
 
-// void testAnimation(int dur, int cur){ 
-//   while(millis() - cur < dur){
-//     checkForUpdates();
-//   	int16_t sx, sy, x, y;
-//     	uint8_t h;
+void testAnimation(int dur, int cur){ 
+  while(millis() - cur < dur){
+    checkForUpdates();
+  	int16_t sx, sy, x, y;
+    	uint8_t h;
 
-//     	FastLED.clear();
+    	FastLED.clear();
 
-//    	h = hue;
-//     	if (counter < 1125)
-//     	{
-//       	// ** Fill LED's with diagonal stripes
-//       	for (x=0; x<(leds.Width()+leds.Height()); ++x)
-//       	{
-//         	leds.DrawLine(x - leds.Height(), leds.Height() - 1, x, 0, CHSV(h, 255, brightness));
-//         	h+=16;
-//       	}
-//     	}
-//     	else
-//     	{
-//       // ** Fill LED's with horizontal stripes
-//       	for (y=0; y<leds.Height(); ++y)
-//       	{
-//         	leds.DrawLine(0, y, leds.Width() - 1, y, CHSV(h, 255, brightness));
-//         	h+=16;
-//       	}
-//     	}
-//     	hue+=4;
+   	h = hue;
+    	if (counter < 1125)
+    	{
+      	// ** Fill LED's with diagonal stripes
+      	for (x=0; x<(MATRIX_WIDTH+MATRIX_HEIGHT); ++x)
+      	{
+        	leds.DrawLine(x - MATRIX_HEIGHT, MATRIX_HEIGHT - 1, x, 0, CHSV(h, 255, brightness));
+        	h+=16;
+      	}
+    	}
+    	else
+    	{
+      // ** Fill LED's with horizontal stripes
+      	for (y=0; y<MATRIX_HEIGHT; ++y)
+      	{
+        	leds.DrawLine(0, y, MATRIX_WIDTH - 1, y, CHSV(h, 255, brightness));
+        	h+=16;
+      	}
+    	}
+    	hue+=4;
 
-//     	if (counter < 125)
-//   		;
-//     	else if (counter < 375)
-//       	leds.HorizontalMirror();
-//     	else if (counter < 625)
-//       	leds.VerticalMirror();
-//     	else if (counter < 875)
-//       	leds.QuadrantMirror();
-//     	else if (counter < 1125)
-//       	leds.QuadrantRotateMirror();
-//     	else if (counter < 1250)
-//       	;
-//     	else if (counter < 1500)
-//       	leds.TriangleTopMirror();
-//     	else if (counter < 1750)
-//       	leds.TriangleBottomMirror();
-//     	else if (counter < 2000)
-//       	leds.QuadrantTopTriangleMirror();
-//     	else if (counter < 2250)
-//       	leds.QuadrantBottomTriangleMirror();
+    	if (counter < 125)
+  		;
+    	else if (counter < 375)
+      	leds.HorizontalMirror();
+    	else if (counter < 625)
+      	leds.VerticalMirror();
+    	else if (counter < 875)
+      	leds.QuadrantMirror();
+    	else if (counter < 1125)
+      	leds.QuadrantRotateMirror();
+    	else if (counter < 1250)
+      	;
+    	else if (counter < 1500)
+      	leds.TriangleTopMirror();
+    	else if (counter < 1750)
+      	leds.TriangleBottomMirror();
+    	else if (counter < 2000)
+      	leds.QuadrantTopTriangleMirror();
+    	else if (counter < 2250)
+      	leds.QuadrantBottomTriangleMirror();
 
-//     	counter++;
-//     	if (counter >= 2250)
-//       	counter = 0;
-//     	FastLED.show();
-//     }
-// }
+    	counter++;
+    	if (counter >= 2250)
+      	counter = 0;
+    	FastLED.show();
+    }
+}
